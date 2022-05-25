71d61c51a8b7e0d8e2774b2bdc4de15d3748c941
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2022-05-25 20:14:26 +0200

    Remove unused default files

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,7 @@ import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
 import { NavBar } from "./navigation/NavBar";
 import { Login } from "./auth/Login";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
 import { Register } from "./auth/Register";
 
 const darkTheme = createTheme({
@@ -22,8 +17,13 @@ const lightTheme = createTheme({
   },
 });
 
-
-
+const MainPage = () => {
+  return (
+    <>
+      <h1>Main Page</h1>
+    </>
+  );
+};
 function App() {
   const [isDark, setIsDark] = useState(false);
   const toggleThemeHandler = () => {
@@ -34,14 +34,12 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <BrowserRouter>
-        <CssBaseline />
-        <NavBar isDark={isDark} setTheme={toggleThemeHandler} />
+          <CssBaseline />
+          <NavBar isDark={isDark} setTheme={toggleThemeHandler} />
           <Routes>
-            <Route path="/regisztracio" element={<Register/>}></Route>
-            <Route path="/bejelentkezes" element={<Login/>} >
-            </Route>
-            <Route path="/" >
-            </Route>
+            <Route path="/regisztracio" element={<Register />}></Route>
+            <Route path="/bejelentkezes" element={<Login />}></Route>
+            <Route path="/" element={<MainPage />}></Route>
           </Routes>
         </BrowserRouter>
       </ThemeProvider>

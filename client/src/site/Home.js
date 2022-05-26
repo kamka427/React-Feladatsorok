799d1c07a1b7d9af1399da595caad654ec020962
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2022-05-26 17:19:56 +0200

    Add placeholder for Home

--- client/src/site/Home.js
+++ client/src/site/Home.js
@@ -1,10 +1,24 @@
-import { AuthStatus } from "../auth/AuthStatus";
-
+import { Container, Typography } from "@mui/material";
 export const Home = () => {
-    return (
-      <>
-        <h1>Főoldal</h1>
-        <AuthStatus/>
-      </>
-    );
-  };
+  return (
+    <>
+      <Container>
+        <Typography variant="h5">Főoldal</Typography>
+        <Typography variant="h6"> A feladat </Typography>
+        <Typography variant="body1">
+          A beadandóban olyan webes alkalmazást kell írnod, amelyben egy
+          tanárnak lehetősége van egy feladatsort összeállítani, pl. óra vagy
+          dolgozat céljából. A tanár létrehoz egy új feladatsort, majd a
+          feladatbankban a feladatok között böngészve egy-egy feladatot hozzáad
+          a szerkesztésre jelölt feladatsorhoz. A feladatok és a feladatsorok
+          listázhatók, részleteik megtekinthetők, a feladatsorok szerkeszthetők.
+          A feladatot React és Redux kombinációjával kell megoldanod, Redux
+          esetében ajánlott a redux toolkit és akár az RTK Query használata.
+          Mivel az alkalmazás több oldalból áll, a react-router használata
+          javasolt. A feladatban adott a szerveroldali REST API, leírását
+          lentebb olvashatjátok, ehhez kell igazodnia a kliensnek.
+        </Typography>
+      </Container>
+    </>
+  );
+};

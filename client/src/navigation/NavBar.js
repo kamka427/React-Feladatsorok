c55c763db0b7255441eee6077f7f0497b19ff41e
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2022-05-27 14:31:09 +0200

    Rework NavBar

--- client/src/navigation/NavBar.js
+++ client/src/navigation/NavBar.js
@@ -1,130 +1,153 @@
-import { Button, Container, Divider, Link, Tab, Tabs } from "@mui/material";
+import {
+  Button,
+  Container,
+  Divider,
+  Link,
+  Stack,
+  Tab,
+  Tabs,
+} from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
-import { useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import { logout, selectLoggedInUser } from "../state/authSlice";
 import { Box } from "@mui/system";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-export const NavBar = ({ isDark, setTheme }) => {
-  const currentRoute = window.location.pathname;
-  const [currentTab, setCurrentTab] = useState(
-    currentRoute !== "/" ? currentRoute : false
-  );
-  const dispatch = useDispatch();
 
+export const NavBar = ({ isDark, setTheme }) => {
   const user = useSelector(selectLoggedInUser);
 
-  const handleChange = (event, newValue) => {
-    setCurrentTab(newValue);
+  const dispatch = useDispatch();
+
+  const [activeTab, setActiveTab] = useState(false);
+
+  const handleTabChange = (event, newValue) => {
+    setActiveTab(newValue);
   };
 
   const resetTab = () => {
-    setCurrentTab(false);
+    setActiveTab(false);
   };
+
+  const currentRoute = window.location.pathname;
 
   return (
     <>
-      <Container
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: 2,
-        }}
-      >
-        <Link
-          variant="h6"
-          component={RouterLink}
-          onClick={resetTab}
-          to="/"
-          color="inherit"
-          underline="none"
-        >
-          React feladatsorok
-        </Link>
-
+      <Container>
         <Box
           sx={{
             display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
           }}
         >
-          <Tabs value={currentTab} onChange={handleChange}>
-            <Tab
-              label="Feladatbank"
-              value="/feladatbank"
+          <Stack
+            direction="row"
+            sx={{
+              alignItems: "center",
+              gap: 2,
+            }}
+          >
+            <Link
+              variant="h6"
               component={RouterLink}
-              to="/feladatbank"
-            />
-            {user && (
+              onClick={resetTab}
+              to="/"
+              color="inherit"
+              underline="none"
+            >
+              React feladatsorok
+            </Link>
+            <Tabs value={activeTab} onChange={handleTabChange}>
               <Tab
-                label="Feladatsoraim"
-                value="/feladatsoraim"
+                label="Feladatbank"
+                value="/feladatbank"
                 component={RouterLink}
-                to="/feladatsoraim"
+                to="/feladatbank"
               />
-            )}
-            {user && (
-              <Tab
-                label="Szerkesztett feladatsor"
-                value="/szerkesztett"
-                component={RouterLink}
-                to="/szerkesztett"
-              />
-            )}
+              {user && (
+                <Tab
+                  label="Feladatsoraim"
+                  value="/feladatsoraim"
+                  component={RouterLink}
+                  to="/feladatsoraim"
+                />
+              )}
+              {user && (
+                <Tab
+                  label="Szerkesztett feladatsor"
+                  value="/szerkesztett"
+                  component={RouterLink}
+                  to="/szerkesztett"
+                />
+              )}
+            </Tabs>
+          </Stack>
+          <Stack direction="row">
             {!user && (
-              <Tab
-                label="Bejelentkezés"
-                value="/bejelentkezes"
-                component={RouterLink}
-                to="/bejelentkezes"
-                sx={{
-                  color: "primary.main",
-                }}
-              />
-            )}
-            {!user && (
-              <Tab
-                label="Regisztráció"
-                value="/regisztracio"
-                component={RouterLink}
-                to="/regisztracio"
-                sx={{
-                  color: "secondary.main",
-                }}
-              />
+              <>
+                <Button
+                  label="Bejelentkezés"
+                  value="/bejelentkezes"
+                  component={RouterLink}
+                  to="/bejelentkezes"
+                  sx={{
+                    color: "primary.main",
+                  }}
+                  onClick={resetTab}
+                >
+                  Bejelentkezés
+                </Button>
+
+                <Button
+                  label="Regisztráció"
+                  value="/regisztracio"
+                  component={RouterLink}
+                  to="/regisztracio"
+                  sx={{
+                    color: "secondary.main",
+                  }}
+                  onClick={resetTab}
+                >
+                  Regisztráció
+                </Button>
+              </>
             )}
             {user && (
-              <Tab
+              <Button
                 label="Kijelentkezés"
                 value="/kijelentkezes"
                 component={RouterLink}
                 to="/"
-                onClick={() => dispatch(logout())}
+                onClick={() => {
+                  dispatch(logout());
+                  resetTab();
+                }}
                 sx={{
                   color: "error.main",
                 }}
               >
                 Kijelentkezés
-              </Tab>
+              </Button>
             )}
-          </Tabs>
-          {user && (
-            <Button component={RouterLink} to="/profil" color="inherit">
-              <AccountCircleIcon />
+            {user && (
+              <Button component={RouterLink} to="/profil" color="inherit">
+                <AccountCircleIcon />
+              </Button>
+            )}
+            <Button
+              value={isDark}
+              selected={isDark}
+              onClick={setTheme}
+              color="inherit"
+            >
+              {isDark ? <LightModeIcon /> : <DarkModeIcon />}
             </Button>
-          )}
-          <Button
-            value={isDark}
-            selected={isDark}
-            onClick={setTheme}
-            color="inherit"
-          >
-            {isDark ? <LightModeIcon /> : <DarkModeIcon />}
-          </Button>
+          </Stack>
         </Box>
       </Container>
     </>

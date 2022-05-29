66947ccd2fe9ac908e0c406834e14a77f82246d8
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2022-05-29 14:29:11 +0200

    Add Roboto font and update Home text

--- client/src/site/Home.js
+++ client/src/site/Home.js
@@ -3,19 +3,15 @@ export const Home = () => (
   <Container>
     <Stack spacing={1} marginTop={2}>
       <Typography variant="h5">Főoldal</Typography>
-      <Typography variant="h6">A feladat</Typography>
+      <Typography variant="h6">Információ</Typography>
       <Typography variant="body1">
-        A beadandóban olyan webes alkalmazást kell írnod, amelyben egy tanárnak
-        lehetősége van egy feladatsort összeállítani, pl. óra vagy dolgozat
-        céljából. A tanár létrehoz egy új feladatsort, majd a feladatbankban a
-        feladatok között böngészve egy-egy feladatot hozzáad a szerkesztésre
-        jelölt feladatsorhoz. A feladatok és a feladatsorok listázhatók,
-        részleteik megtekinthetők, a feladatsorok szerkeszthetők. A feladatot
-        React és Redux kombinációjával kell megoldanod, Redux esetében ajánlott
-        a redux toolkit és akár az RTK Query használata. Mivel az alkalmazás
-        több oldalból áll, a react-router használata javasolt. A feladatban
-        adott a szerveroldali REST API, leírását lentebb olvashatjátok, ehhez
-        kell igazodnia a kliensnek.
+        Az oldal segítségével egyszerűen tud feladatsorokat összeállítani órai
+        munka vagy dolgozatiratás céljából. Regisztráció, illetve bejelentkezés
+        után, a feladatbankból feladatokat válogatva, új feladatsorokat hozhat
+        létre, illetve meglévőket módosíthat. A feladatbankon keresztül a
+        feladatok eltávolítása is lehetséges a szerkesztett feladatsorból. A
+        profil fülön megtekintheti adatait. A hold ikonra kattintva
+        változtathatja az oldal témáját.
       </Typography>
     </Stack>
   </Container>
